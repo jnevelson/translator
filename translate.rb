b6361ece7007c32f7f08e32292a21a180433f35e
Jonathan Nevelson <jnevelson@gmail.com>
2011-11-04 10:45:24 -0700

    Super weird bug
    Putting it on another branch because it's specific to a certain local
    as far as I can tell

--- translate.rb
+++ translate.rb
@@ -3,19 +3,26 @@ require 'google_translate'
 
 def translate_hash(hash)
   {}.tap do |hsh|
-    hash.each do |k, v|
-      case v
+    hash.each do |key, val|
+      case val
       when Hash
-        hsh[k] = translate_hash(v)
+        hsh[key] = translate_hash(val)
       when Array
-        hsh[k] = v.map { |a| translate(a) }
+        hsh[key] = val.map { |v| translate(v) }
       else
         begin
-          tr = translate(v)
-          puts "#{@counter+=1}. #{v} -- #{tr}"
-          hsh[k] = tr
+          tr = translate(val)
+          hsh[key] = tr
+          puts "#{@counter+=1}. #{val} -- #{tr}"
         rescue Exception => e
-          return hsh
+          # Super weird bug with certain words in zh_cn locale...
+          # https://github.com/shvets/google-translate/issues/6
+          if exceptions[val.downcase]
+            hsh[key] = exceptions[val]
+            puts "#{@counter+=1}. #{val}"
+          else
+            raise "#{e} -- key: #{key} -- val: #{val}"
+          end
         end
       end
     end
@@ -23,7 +30,22 @@ def translate_hash(hash)
 end
 
 def translate(word)
-  @translator.translate(:en, @locale, word)[0].force_encoding('utf-8')
+  @translator.translate(:en, @locale, word)[0].encode("utf-8")
+end
+
+def exceptions
+  {
+    "slow" => "\xE7\xBC\x93\xE6\x85\xA2",
+    "fast" => "\xE5\xBF\xAB",
+    "high" => "\xE9\xAB\x98",
+    "crouch" => "\xE8\xB9\xB2\xE4\xBC\x8F",
+    "speed" => "\xE9\x80\x9F\xE5\xBA\xA6",
+    "short" => "\xE7\x9F\xAD",
+    "long" => "\xE9\x95\xBF",
+    "quick" => "\xE5\xBF\xAB\xE9\x80\x9F",
+    "head" => "\xE5\xA4\xB4",
+    "down" => "\xE4\xB8\x8B\xE9\x99\x8D"
+  }
 end
 
 @locale = ARGV[0]
